6cf8cbbf0c606fd43a1f386e1c01c6942d7430b5
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-06-17 16:51:54 +0800

    Initial

---
+++ Data/PizzaService.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        private List<Pizza> pizzas = new();
+
+        public async Task<List<Pizza>> GetPizzasAsync()
+        {
+            
+            // Call your data access technology here
+            pizzas.AddRange(new List<Pizza>
+            {
+                new Pizza { PizzaId = 1, Name = "Hawaian", Description = "Descrip", Price = 12.5m, Vegan = false, Vegetarian = false}
+
+            });
+
+            // Introduce a 5-second delay
+            await Task.Delay(5000); // 5000 milliseconds = 5 seconds
+
+            return pizzas;
+        }
+
+        public Task<List<Pizza>> GetPizzas()
+        {
+
+            // Call your data access technology here
+            pizzas.AddRange(new List<Pizza>
+            {
+                new Pizza { PizzaId = 1, Name = "Hawaian", Description = "Descrip", Price = 12.5m, Vegan = false, Vegetarian = false}
+
+            });
+
+            // Introduce a 5-second delay
+            Task.Delay(5000); // 5000 milliseconds = 5 seconds
+
+            return Task.FromResult(pizzas);
+        }
+    }
+}
