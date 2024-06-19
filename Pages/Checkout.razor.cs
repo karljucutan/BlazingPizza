497f917cf833660de4e20d97412017f439c8d887
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-06-19 17:01:35 +0800

    Add server-side and client-side data validation to the address form using DataAnnotation.

--- Pages/Checkout.razor.cs
+++ Pages/Checkout.razor.cs
@@ -12,24 +12,26 @@ namespace BlazingPizza.Pages
 
         private async Task PlaceOrder() // private access modifier by default if not explicitly indicated.
         {
+            isSubmitting = true;
             var response = await HttpClient.PostAsJsonAsync($"{NavigationManager.BaseUri}orders", OrderState.Order);
             var newOrderId = await response.Content.ReadFromJsonAsync<int>();
             OrderState.ResetOrder();
+            isSubmitting = false;
             NavigationManager.NavigateTo($"myorders/{newOrderId}");
         }
 
-        private async Task CheckSubmission(EditContext editContext)
-        {
-            isSubmitting = true;
-            var model = editContext.Model as Address;
-            isError = string.IsNullOrWhiteSpace(model?.Name)
-                || string.IsNullOrWhiteSpace(model?.Line1)
-                || string.IsNullOrWhiteSpace(model?.PostalCode);
-            if (!isError)
-            {
-                await PlaceOrder();
-            }
-            isSubmitting = false;
-        }
+        //private async Task CheckSubmission(EditContext editContext)
+        //{
+        //    isSubmitting = true;
+        //    var model = editContext.Model as Address;
+        //    isError = string.IsNullOrWhiteSpace(model?.Name)
+        //        || string.IsNullOrWhiteSpace(model?.Line1)
+        //        || string.IsNullOrWhiteSpace(model?.PostalCode);
+        //    if (!isError)
+        //    {
+        //        await PlaceOrder();
+        //    }
+        //    isSubmitting = false;
+        //}
     }
 }
