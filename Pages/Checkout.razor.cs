16fd4ea1d10b6a22201d372b29d91337671b8d16
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-06-19 17:15:25 +0800

    Enable the submit button when all fields are correct

--- Pages/Checkout.razor.cs
+++ Pages/Checkout.razor.cs
@@ -7,15 +7,24 @@ namespace BlazingPizza.Pages
     public partial class Checkout
     {
         private Order Order => OrderState.Order; // private access modifier by default if not explicitly indicated.
-        private bool isSubmitting;
         bool isError = false;
+        private EditContext editContext;
+
+        protected override void OnInitialized()
+        {
+            editContext = new(Order.DeliveryAddress);
+            editContext.OnFieldChanged += HandleFieldChanged;
+        }
+
+        private void HandleFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            isError = !editContext.Validate();
+            StateHasChanged();
+        }
 
         private async Task PlaceOrder() // private access modifier by default if not explicitly indicated.
         {
-            isError = false;
-            isSubmitting = true;
-            var response = await HttpClient.PostAsJsonAsync(
-                $"{NavigationManager.BaseUri}orders", OrderState.Order);
+            var response = await HttpClient.PostAsJsonAsync(NavigationManager.BaseUri + "orders", OrderState.Order);
             var newOrderId = await response.Content.ReadFromJsonAsync<int>();
             OrderState.ResetOrder();
             NavigationManager.NavigateTo($"myorders/{newOrderId}");
@@ -24,6 +33,11 @@ namespace BlazingPizza.Pages
         protected void ShowError()
         {
             isError = true;
+        }
+
+        public void Dispose()
+        {
+            editContext.OnFieldChanged -= HandleFieldChanged;
         }
     }
 }
