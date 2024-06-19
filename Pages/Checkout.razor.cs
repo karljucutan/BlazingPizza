f873d7fe8c6ccc766b291fb7a125a7b98f561259
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-06-19 16:35:21 +0800

    AspNet Component Forms. EditForm and InputText.

--- Pages/Checkout.razor.cs
+++ Pages/Checkout.razor.cs
@@ -1,5 +1,6 @@
 using BlazingPizza.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace BlazingPizza.Pages
 {
@@ -7,14 +8,28 @@ namespace BlazingPizza.Pages
     {
         private Order Order => OrderState.Order; // private access modifier by default if not explicitly indicated.
         private bool isSubmitting;
+        bool isError = false;
 
-        async Task PlaceOrder() // private access modifier by default if not explicitly indicated.
+        private async Task PlaceOrder() // private access modifier by default if not explicitly indicated.
         {
-            isSubmitting = true;
             var response = await HttpClient.PostAsJsonAsync($"{NavigationManager.BaseUri}orders", OrderState.Order);
             var newOrderId = await response.Content.ReadFromJsonAsync<int>();
             OrderState.ResetOrder();
             NavigationManager.NavigateTo($"myorders/{newOrderId}");
+        }
+
+        private async Task CheckSubmission(EditContext editContext)
+        {
+            isSubmitting = true;
+            var model = editContext.Model as Address;
+            isError = string.IsNullOrWhiteSpace(model?.Name)
+                || string.IsNullOrWhiteSpace(model?.Line1)
+                || string.IsNullOrWhiteSpace(model?.PostalCode);
+            if (!isError)
+            {
+                await PlaceOrder();
+            }
+            isSubmitting = false;
         }
     }
 }
